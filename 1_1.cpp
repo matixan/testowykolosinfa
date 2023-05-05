1e9ba1be849d25cc182f0af4a98323e213ebb19e
Mateusz Czarnecki <matixan02@gmail.com>
2023-05-05 18:58:31 +0200

    init
    init

---
+++ 1_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (b < 0.0 || d < 0.0){
+        cout << "nie dzielimy przez 0" << endl;
+        return 0;
+    }
+    cout << (a/b+c/d) << endl;
+}
