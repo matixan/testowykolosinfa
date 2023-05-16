ee3541e66d5d820a6240b31eb2072b4a7c4b9295
Mateusz Czarnecki <matixan02@gmail.com>
2023-05-16 18:52:36 +0200

    update

--- KOLOS_1D/z2.cpp
+++ KOLOS_1D/z2.cpp
@@ -19,19 +19,34 @@ using namespace std;
 
 void czastkowanie(string zdanie){ // z uwagi na to ze nie mozna uzyc petli w funkcji rekurencyjnej to trzeba uzyc funkcji substr() ktora zwraca krotszego stringa
     if (zdanie.length() == 0) return; // warunek konca rekurencji// zwraca 0 bo nie ma znakow
-    bool znaleziono = false;
-    if (zdanie[0] == '.' || zdanie[0] == ',' || zdanie[0] == ':' || zdanie[0] == ';') znaleziono = true; // jezeli znak jest interpunkcyjny to wypisuje enter
     cout << zdanie[0]; // wypisuje aktualnie pierwszy znak
-    if (znaleziono) {
-        cout<<zdanie.length()<<endl;
-    } 
+    if (zdanie[0] == '.' || zdanie[0] == ',' || zdanie[0] == ':' || zdanie[0] == ';') cout<<zdanie.length()<<endl; // jezeli znak jest interpunkcyjny to wypisuje enter
     czastkowanie(zdanie.substr(1));
     return;
+}
+
+// czas mi sie skonczyl ale tu jest kod ktory dziala ale musial chatGPT pomoc xdd
+
+void czastkowanie2(string zdanie, string wiersz = "") {
+    if (zdanie.length() == 0) {
+        cout << wiersz << wiersz.length() << endl; // wyświetlanie ostatniego wiersza i ilości znaków w nim
+        return; // warunek konca rekurencji
+    }
+    if (zdanie[0] == '.' || zdanie[0] == ',' || zdanie[0] == ':' || zdanie[0] == ';') { // sprawdzanie, czy znak jest interpunkcyjny
+        cout << wiersz << zdanie[0] << wiersz.length()+1 << endl; // wyświetlanie dotychczasowego wiersza, znaku interpunkcyjnego i dlugosci wiersza
+        wiersz = ""; // resetowanie wiersza
+    } else {
+        wiersz += zdanie[0]; // dodawanie znaku do aktualnego wiersza
+    }
+    czastkowanie2(zdanie.substr(1), wiersz); // wywołanie rekurencyjne z podciągiem od drugiego znaku
 }
 
 int main(){
     string zdanie;
     getline(cin, zdanie);
     czastkowanie(zdanie); // przyjmuje ze jednak liczenie znakow odbywa sie w rekurencji a nie w programie bo inaczej nie ma sensu
+    cout<<endl<<endl;
+    czastkowanie2(zdanie); // dzialajaca z pomoca chatGPT
     return 0; //cout zeby wyswietlic ostatnie zliczenie znakow
 }
+
